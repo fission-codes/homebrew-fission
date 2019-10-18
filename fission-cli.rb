74931dcac9164b5097f7aa7515d6696ef0f0a2b2
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-10-18 21:41:52 +0200

    FissionCli formula

---
+++ fission-cli.rb
@@ -0,0 +1,26 @@
+class FissionCli < Formula
+  desc "Deploy live from your laptop, and other web3 tricks for a web2 world"
+  homepage "https://fission.codes"
+  url "https://github.com/fission-suite/web-api/releases/download/1.15.0/macos-cli"
+  sha256 "72100096d0f4825b221c1a6d55184831a8675013de5547e7b87d728f5a28e3ba"
+
+  depends_on "ipfs"
+
+  def install
+    bin.install "macos-cli"
+    mv bin/"macos-cli", bin/"fission"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test web-api`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
