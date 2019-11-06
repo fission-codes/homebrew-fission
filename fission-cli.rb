7c6289ee625074f2e8fdcd39f2c2f20d1a1b078c
Ben Church <ben.douglas.church@gmail.com>
2019-11-06 17:52:59 -0600

    Update to version 1.21.1 (#1)

--- fission-cli.rb
+++ fission-cli.rb
@@ -1,14 +1,14 @@
 class FissionCli < Formula
   desc "Deploy live from your laptop, and other web3 tricks for a web2 world"
   homepage "https://fission.codes"
-  url "https://github.com/fission-suite/cli/releases/download/1.20.0/macos-cli"
-  sha256 "82d4e19211c27dab590f1701b39841a1fbd945fd9670eba9fd9c23000cae8e04"
+  url "https://github.com/fission-suite/cli/releases/download/v1.21.1/macOS-10.14.zip"
+  sha256 "8941d62e8c7e0d5b0d9d476cf89d15202a0b858bfead2334e10473eb468b8b90"
 
   depends_on "ipfs"
 
   def install
-    bin.install "macos-cli"
-    mv bin/"macos-cli", bin/"fission"
+    bin.install "fission-cli-exe"
+    mv bin/"fission-cli-exe", bin/"fission"
     ohai "Please ensure IPFS is running with: brew services start ipfs"
   end
 
