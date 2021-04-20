7508e43fe57a90a3455b48c5e6a3e2fa3e72faf1
James Walker <walkah@walkah.net>
2021-04-20 16:02:17 +0000

    fission-cli: add 2.10.2 bottle.

--- Formula/fission-cli.rb
+++ Formula/fission-cli.rb
@@ -5,6 +5,12 @@ class FissionCli < Formula
   sha256 "9803da4b5c6d06b53d7c3fb2abdb79fa08091588d2e82b58b5547b73ebc6709b"
   license "AGPL-3.0-or-later"
 
+  bottle do
+    root_url "https://github.com/fission-suite/homebrew-fission/releases/download/fission-cli-2.10.2"
+    sha256 cellar: :any,                 catalina:     "32eb34015170ec117aa8bacd9bed23cacccb9c15791872ee6f46442a6503c0d7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "72755d734868addb0b410adb26493d79a110316810034c6ff3f1fc89c8c40b2b"
+  end
+
   depends_on "ghc" => :build
   depends_on "haskell-stack" => :build
   depends_on "openssl"
