d848e9ec8947caca892b11f3323df0db0027ed8c
James Walker <walkah@walkah.net>
2021-04-23 12:10:25 -0400

    install shell completions

--- Formula/fission-cli.rb
+++ Formula/fission-cli.rb
@@ -4,6 +4,7 @@ class FissionCli < Formula
   url "https://github.com/fission-suite/fission/archive/refs/tags/2.10.2.tar.gz"
   sha256 "9803da4b5c6d06b53d7c3fb2abdb79fa08091588d2e82b58b5547b73ebc6709b"
   license "AGPL-3.0-or-later"
+  revision 1
 
   bottle do
     root_url "https://github.com/fission-suite/homebrew-fission/releases/download/fission-cli-2.10.2"
@@ -21,6 +22,10 @@ class FissionCli < Formula
   def install
     system "stack", "install", "--system-ghc", "--no-install-ghc", "--skip-ghc-check", "--local-bin-path=#{bin}",
       "fission-cli:fission"
+
+    (bash_completion/"fission").write `#{bin}/fission --bash-completion-script #{bin}/fission`
+    (fish_completion/"fission.fish").write `#{bin}/fission --fish-completion-script #{bin}/fission`
+    (zsh_completion/"_fission").write `#{bin}/fission --zsh-completion-script #{bin}/fission`
   end
 
   test do
